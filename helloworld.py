e644a086db090756eb3d0d301b87461a1a99711b
ratalabhargavi <ratalabhargavi@gmail.com>
2024-02-29 19:32:31 +0530

    Add helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,5 @@
+DELL@DESKTOP-HF5SRO3 MINGW64 ~/.ssh/exp1 (master)
+$ git clone git@github.com:ratalabhargavi/exp1.git
+fatal: destination path 'exp1' already exists and is not an empty directory.iELL@DESKTOP-HF5SRO3 MINGW64 ~/.ssh/exp1 (master)
+$ git clone git@github.com:ratalabhargavi/exp1.git
+fatal: destination path 'exp1' already exists and is not an empty directory.
